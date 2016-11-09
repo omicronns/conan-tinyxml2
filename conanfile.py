f2dc519f5c42aaf7dbaa77faabf62c835b24a8d2
Emil Bostijancic <emil.bostijancic@gmail.com>
2016-11-09 21:06:23 +0000

    fixed build problem on linux.

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class TinyxmlConan(ConanFile):
     url = "https://github.com/ebostijancic/conan-tinyxml2"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
-    default_options = "shared=False"
+    default_options = "shared=True"
     generators = "cmake"
 
     def source(self):
@@ -25,9 +25,9 @@ class TinyxmlConan(ConanFile):
     def package(self):
         self.copy("*.h", dst="include/tinyxml2", src="tinyxml2")
         self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.so*", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*.a*", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["tinyxml2.4.0.1"]
+        self.cpp_info.libs = ["tinyxml2"]
