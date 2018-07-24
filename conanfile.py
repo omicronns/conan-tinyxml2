2e06d102d8887be945c38faa97a894340629ba9e
omicronns <omicronns@gmail.com>
2018-07-25 01:27:23 +0200

    update

--- conanfile.py
+++ conanfile.py
@@ -4,30 +4,35 @@ import os
 
 class TinyxmlConan(ConanFile):
     name = "tinyxml2"
-    version = "4.0.1"
+    version = "0.1.0"
     license = "MIT"
-    url = "https://github.com/ebostijancic/conan-tinyxml2"
+    url = "https://github.com/omicronns/conan-tinyxml2"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=True"
+    options = {
+        "version": ["6.2.0"],
+    }
+    default_options = (
+        "version=" + options["version"][-1],
+    )
     generators = "cmake"
 
     def source(self):
-       self.run("git clone https://github.com/leethomason/tinyxml2")
-       self.run("cd tinyxml2 && git checkout %s" % self.version)
+        self.run("git clone --branch {} --single-branch https://github.com/leethomason/tinyxml2 sources".format(self.options.version))
 
     def build(self):
-        cmake = CMake(self.settings)
-        shared = "-DBUILD_SHARED_LIBS=ON" if self.options.shared else ""
-        self.run('cmake tinyxml2 %s %s' % (cmake.command_line, shared))
-        self.run("cmake --build . %s" % cmake.build_config)
+        cmake = CMake(self)
+        source_dir = os.path.join(self.build_folder, "sources")
+        cmake.configure(source_dir=source_dir)
+        cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include/tinyxml2", src="tinyxml2")
+        self.copy("*.h", dst="include/tinyxml2", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so*", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a*", dst="lib", keep_path=False)
+        self.copy("*.lib*", dst="lib", keep_path=False)
+        self.copy("*.pdb", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["tinyxml2"]
